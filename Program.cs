327dec7ba4a749a22b5ef9f0ae4661b6b9e9fb02
Delta <Athlmyn@outlook.com>
2023-06-03 13:37:29 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿namespace InventoryManagement.ConsoleApp {
+    internal class Program {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( "Hello, World!" );
+        }
+    }
+}
